4f1d96a375b6f6eb88841e4c21e55d0d5943a3f7
JRSmile <jrsmile@users.noreply.github.com>
2018-07-01 23:54:51 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php 
+?>
+<html>
+<head>
+<style>
+a:link {color: #66ff66}
+a:visited {color: #66ff66}
+body{color:green}
+body{background-color: #000000}
+.container {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  position: fixed;
+  align-items: center;
+  justify-content: center;
+}
+body {background: url(bg.jpg) no-repeat center center fixed; 
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+</style>
+</head>
+<body>
+<div class="container">
+<div><h1>iPXE Tiny Core Linux 9 boot <br />via chain http://jrsmile.111mb.de/boot.txt</h1>
+<a href="https://github.com/jrsmile/ipxe-tiny-core">more about the project</a></div>
+</div>
+</body>
+</html>
